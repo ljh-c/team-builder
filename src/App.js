f71c03ae4432975071000c80804c80a5637fd5ff
ljh-c <57099181+ljh-c@users.noreply.github.com>
2019-12-18 08:14:51 -0800

    form adds team member to roster

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React, { useState } from 'react';
 import './App.css';
+import Form from './Form';
 
 function App() {
-  const [teamMembers] = useState([]);
+  const [roster, setRoster] = useState([]);
+
+  const [memberData, setMemberData] = useState({
+    name: '', 
+    password: '', 
+    role: '', 
+    isAdmin: false
+  });
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setRoster([...roster, memberData]);
+      
+  };
 
   return (
     <div className="App">
-      {teamMembers}
+      <h1>Add Team Member</h1>
+      <Form memberData={memberData} setMemberData={setMemberData} handleSubmit={handleSubmit} />
+      <h2>Team</h2>
+      {roster.map((member, index) => {
+        return (
+          <div key={index}>
+            <p>{member.name}</p>
+            <p>{member.password}</p>
+            <p>{member.role}</p>
+            {member.hasOwnProperty('isAdmin') ? <p>admin</p> : null}
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+function Form({ memberData, setMemberData, handleSubmit }) {
+  
+  const handleInputChange = event => {
+    setMemberData({
+      ...memberData, 
+      [event.target.name]: event.target.value
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name: <input 
+          type="text" 
+          placeholder="first name"
+          name="name" 
+          onChange={handleInputChange} 
+          value={memberData.name}/>
+      </label>
+
+      <label>
+        Password: <input 
+          type="text" 
+          name="password" 
+          onChange={handleInputChange} 
+          value={memberData.password}/>
+      </label>
+
+      <label>
+        Role: <select 
+            name="role" 
+            onChange={handleInputChange}
+            value={memberData.role}>
+          <option>Frontend Engineer</option>
+          <option>Backend Engineer</option>
+          <option>UX Designer</option>
+          <option>Data Scientist</option>
+        </select>
+      </label>
+
+      <label>
+        Admin: <input type="checkbox" name="isAdmin" onChange={handleInputChange} value={memberData.isAdmin}/>
+      </label>
+
+      <button>Submit</button>
+    </form>
+  );
+}
+
+export default Form;
